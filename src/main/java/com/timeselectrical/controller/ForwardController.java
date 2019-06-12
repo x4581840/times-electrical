7ed403e19659c1ad964d0eaa31e8c57619e78125
liaojieping <5677802@qq.com>
2019-06-12 19:41:01 +0800

    正向查询页面

--- src/main/java/com/timeselectrical/controller/ForwardController.java
+++ src/main/java/com/timeselectrical/controller/ForwardController.java
@@ -1,6 +1,7 @@
 package com.timeselectrical.controller;
 
 
+import com.timeselectrical.model.Student;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
@@ -49,5 +50,45 @@ public class ForwardController {
     public String tab2(HttpServletRequest request) {
 
         return "link_forward";
+    }
+    @ResponseBody
+    @RequestMapping("/getList")
+    public List<Student> getList(HttpServletRequest request) {
+       String aa=request.getParameter("searchContext");
+        System.out.print("参数为"+aa);
+        List<Student> result = new ArrayList<>();
+        Student student = new Student(1, 1, "A", "1", "1");
+        result.add(student);
+
+        student = new Student(2, 2, "B", "2", "2");
+        result.add(student);
+
+        student = new Student(3, 3, "C", "3", "3");
+        result.add(student);
+
+        student = new Student(4, 4, "D", "4", "4");
+        result.add(student);
+
+        student = new Student(5, 5, "E", "5", "5");
+        result.add(student);
+
+        student = new Student(6, 6, "F", "6", "6");
+        result.add(student);
+
+        student = new Student(7, 7, "G", "7", "7");
+        result.add(student);
+
+        student = new Student(8, 8, "H", "8", "8");
+        result.add(student);
+
+        student = new Student(9, 9, "I", "9", "9");
+        result.add(student);
+
+        student = new Student(10, 10, "J", "10", "11");
+        result.add(student);
+
+        student = new Student(11, 11, "K", "11", "11");
+        result.add(student);
+        return result;
     }
 }
---
+++ src/main/resources/static/js/forward_tab3.js
@@ -0,0 +1,87 @@
+
+var $table3;
+//初始化bootstrap-table的内容
+function InitTable3(parm) {
+    //记录页面bootstrap-table全局变量$table，方便应用
+    var queryUrl = '/forward/getList';
+    $table3= $('#table3').bootstrapTable({
+        url: queryUrl,                      //请求后台的URL（*）
+        method: 'POST',                      //请求方式（*）
+        //toolbar: '#toolbar',              //工具按钮用哪个容器
+        striped: true,                      //是否显示行间隔色
+        cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+        pagination: true,                   //是否显示分页（*）
+        //sortable: true,                     //是否启用排序
+        //sortOrder: "asc",                   //排序方式
+
+
+        sidePagination: "client",           //分页方式：client客户端分页，server服务端分页（*）
+        pageNumber: 1,                      //初始化加载第一页，默认第一页,并记录
+        pageSize: 10,                     //每页的记录行数（*）
+        pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+        search: false,                      //是否显示表格搜索
+        strictSearch: true,
+        //showColumns: true,                  //是否显示所有的列（选择显示的列）
+        //showRefresh: true,                  //是否显示刷新按钮
+        minimumCountColumns: 2,             //最少允许的列数
+        //clickToSelect: true,                //是否启用点击选中行
+        //height: 500,                      //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+        uniqueId: "ID",                     //每一行的唯一标识，一般为主键列
+        showToggle: false,                   //是否显示详细视图和列表视图的切换按钮
+        cardView: false,                    //是否显示详细视图
+        detailView: false,                  //是否显示父子表
+        //得到查询的参数
+        queryParams : function (params) {
+            //这里的键的名字和控制器的变量名必须一致，这边改动，控制器也需要改成一样的
+            var temp = {
+                rows: params.limit,                         //页面大小
+                page: (params.offset / params.limit) + 1,   //页码
+                sort: params.sort,      //排序列名
+                sortOrder: params.order //排位命令（desc，asc）
+            };
+            console.log("test=="+$("#dependOn").val());
+            return temp;
+        },
+            columns: [{field: 'id', title: '物料编码', sortable: true, halign: 'left',align: 'left',cellStyle:function(value,row,index){
+                    return {css:{"width":"150px"}}
+                },formatter: function(value, row, index) {
+                    var actions = [];
+                    actions.push('<div style="margin-left: -10px"><input type="checkbox" name="inlineCheckbox1" style="width: 40px;margin-left: 40px" value="'+value+'" onclick="chooseOne(this)">'+'<span>'+value+'</span></div>');
+
+                    return actions.join('');
+                }
+                },
+                {field: 'age', title: '故障物料名称', sortable: true, halign: 'left', cellStyle: function(value, row, index) {
+
+                        return {css:{"width":"150px"}}
+                    }},
+
+
+                 ],
+        onLoadSuccess: function () {
+
+        },
+        onLoadError: function () {
+            showTips("数据加载失败！");
+        }
+    });
+};
+
+// $(function(){
+//     InitMainTable();
+// });
+
+function tab3_query_button() {
+
+    var serach = $("#serach").val();
+    alert(serach);
+    var  from=$("#from").val();
+    var  to=$("#to").val();
+    var data={
+        "searchContext":serach,
+        "fromDate":from,
+        "toDate":to
+    }
+
+    InitTable3(data);
+}
