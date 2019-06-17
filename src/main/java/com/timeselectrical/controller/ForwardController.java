c29ae4e608718edd2d7feda3a38a6b137e4aa471
jianyong.long <414934645@qq.com>
2019-06-17 15:44:33 +0800

    代码添加

--- src/main/java/com/timeselectrical/controller/ForwardController.java
+++ src/main/java/com/timeselectrical/controller/ForwardController.java
@@ -16,6 +16,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -66,9 +67,11 @@ public class ForwardController {
     }
 
     @RequestMapping(value = "/getLinkForwardPage",method = RequestMethod.GET)
-    public String tab2(@RequestParam(name = "fGzpthwzbm") String fGzpthwzbm, HttpServletRequest request) {
-        LOGGER.info("fGzpthwzbm:"+fGzpthwzbm);
-        request.setAttribute("fGzpthwzbm", fGzpthwzbm);
+    public String tab2(@RequestParam(name = "fGzpthwzbm", required = false) String fGzpthwzbm, HttpServletRequest request) {
+        if(!StringUtils.isEmpty(fGzpthwzbm)) {
+            LOGGER.info("fGzpthwzbm:" + fGzpthwzbm);
+            request.setAttribute("fGzpthwzbm", fGzpthwzbm);
+        }
         return "link_forward";
     }
     /**
