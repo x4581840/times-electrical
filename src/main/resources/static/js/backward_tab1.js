1bd7def6cbee80492ca2dcce24e2d35b8dfaabae
jianyong.long <414934645@qq.com>
2019-06-22 16:43:16 +0800

    代码提交

--- src/main/resources/static/js/backward_tab1.js
+++ src/main/resources/static/js/backward_tab1.js
@@ -125,15 +125,19 @@ function backward_tab1_query_button() {
 
 //多选取值
 function getMultiSelectVal() {
-    var result = $.map($("#componentLocSelect").select2('data'), function(value) {
-        return value.text;
-    });//.join(",");
+    // var result = $.map($("#componentLocSelect").select2('data'), function(value) {
+    //     return value.text;
+    // });//.join(",");
+    //$(".chosen option:selected").text(); 获取的多选值是拼接在一起的，没法区分开
+    var result = $(".chosen option:selected").map(function () {
+        return this.text;
+    }).get();
     console.log("multiSelectVal:"+result);
     return result;
 }
 
 $(function () {
-    $("#componentLocSelect").select2({
+    /*$("#componentLocSelect").select2({
         ajax:{
             url: '/backward/getLocSelectData',
             dataType: 'json',  //比较重要，没有的话返回json数据会无法识别，搜索失败
@@ -164,8 +168,101 @@ $(function () {
         placeholder: '物料描述查询',
         minimumInputLength: 1, //至少要输入一个字符才能去查询
         maximumSelectionLength: 10,  //最多能够选择的个数
-        allowClear: true,
-        multiple: true // 开启多选
+        //allowClear: true,
+        multiple: true, // 开启多选
+        closeOnSelect: true,
+        openOnEnter: true
+    }).trigger("change");*/
+
+
+    /*$(".selectpicker").selectpicker({
+        noneSelectedText : '物料描述查询'    //默认显示内容
     });
-})
+
+    loadSelectdata();    //执行此函数，从后台获取数据，拼接成option标签，添加到select的里面
+
+    //初始化刷新数据
+    $(window).on('load', function() {
+        $('.selectpicker').selectpicker('refresh');
+    });*/
+
+
+    $(".chosen").chosen({
+        no_results_text: "没有找到结果！",//搜索无结果时显示的提示
+        search_contains:true,   //关键字模糊搜索。设置为true，只要选项包含搜索词就会显示；设置为false，则要求从选项开头开始匹配
+        allow_single_deselect:true, //单选下拉框是否允许取消选择。如果允许，选中选项会有一个x号可以删除选项
+        width: '250px',
+        // disable_search: false, //禁用搜索。设置为true，则无法搜索选项。
+        // disable_search_threshold: 0, //当选项少等于于指定个数时禁用搜索。
+        // inherit_select_classes: true, //是否继承原下拉框的样式类，此处设为继承
+        // //placeholder_text_single: '选择国家', //单选选择框的默认提示信息，当选项为空时会显示。如果原下拉框设置了data-placeholder，会覆盖这里的值。
+        // width: '400px', //设置chosen下拉框的宽度。即使原下拉框本身设置了宽度，也会被width覆盖。
+        // max_shown_results: 1000, //下拉框最大显示选项数量
+        // display_disabled_options: false,
+        // single_backstroke_delete: false, //false表示按两次删除键才能删除选项，true表示按一次删除键即可删除
+        // case_sensitive_search: false, //搜索大小写敏感。此处设为不敏感
+        // group_search: false, //选项组是否可搜。此处搜索不可搜
+        // include_group_label_in_selected: false //选中选项是否显示选项分组。false不显示，true显示。默认false。
+    });
+
+    jQuery.ajax({
+            url: '/backward/getLocSelectData',//这个url用来获取所有数据l
+            dataType: "json",
+            success: function (data) {
+            var html ='';
+            //jQuery(".chosen").chosen({maxHeight: 200});
+            for (var i = 0; i < data.results.length; i++) {
+                /*html+='<option value="'+result[i].id+'">'+result[i].name+'</option>'*/
+                html+='<option value="'+data.results[i].id+'">'+data.results[i].text+'</option>'
+            }
+            jQuery(".chosen").append(html);
+            $('.chosen').trigger('chosen:updated');
+        }
+    });
+
+
+    //data-init：设置列表初始被选中的项目，传递值为keyField对应的内容
+    /*$('#componentLocSelect').bComboSelect({
+        showField : 'text', //设置下拉列表中显示文本的列
+        keyField : 'text', //设置下拉列表项目中项目的KEY值，用于提交表单
+        data : '/backward/getLocSelectData', //数据源，可以是JSON数据格式，也可以是URL
+        params : function(){
+            //return {'name':'aa','sex':1};
+            console.log("params");
+        },
+        //格式化显示项目，提供源数据进行使用
+        formatItem : function(data){
+            return data.text;
+        },
+        //启用多选模式
+        multiple : true,
+        callback : function(data){
+            //$('#callbackLog').append(data.desc + '(' + data.name + ')');
+            console.log("call back data="+data)
+        }
+    });*/
+});
+
+function loadSelectdata(){
+    $.ajax({
+        url : "/backward/getLocSelectData",    //后台controller中的请求路径
+        type : 'GET',
+        async : false,
+        datatype : 'json',
+        success : function(data) {
+            if(data.results){
+                var parknames =[];
+                for(var i=0,len=data.results.length;i<len;i++){
+                    var parkdata = data.results[i];
+                    //拼接成多个<option><option/>
+                    parknames.push('<option value="'+parkdata.id+'">'+parkdata.text+'</option>')
+                }
+                $("#componentLocSelect").html(parknames.join(' '));    //根据parkID(根据你自己的ID写)填充到select标签中
+            }
+        },
+        error : function() {
+            alert('查询停车场名称出错');
+        }
+    });
+}
 
